bfd250f6f4b0dce81fb57e2d3d57517f17c5546d
Kito Joseph <kitojoseph@gmail.com>
2014-07-19 09:11:21 -0400

    Added file logging

--- logger.c
+++ logger.c
@@ -37,11 +37,29 @@ int print_syslog(char *string){
 	return 0;
 }
 
+int print_logfile(FILE *f, char *string){
+	char timestring[100];
+	time_t currenttime = time(0);
+	strftime(timestring,sizeof(timestring),"%c",localtime(&currenttime));
+	fprintf(f,"%s %s \n",timestring,string);	
+	return 0;
+}
 int main(){
 	
+	char logfile[]= "./text-data-files/logfile.txt";
 	
+	FILE *fp;
+	
+	fp = fopen(logfile,"a+");   //Initiall caused a segfault because no error checking
+	
+	if(fp == NULL){
+		perror("Errror with fopen");
+	}
+
+	print_logfile(fp,"And we printing to the log file");
 	print_log("Error you machine is going down");
 	print_syslog("Holy moly you got another error");
+	fclose(fp);
 	return 0;
 
 }
