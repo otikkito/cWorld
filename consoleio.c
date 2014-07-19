12618991a6b4e5f46008fff21fda90415a7e3d4c
Kito Joseph <kitojoseph@gmail.com>
2014-07-19 07:13:57 -0400

    Added console interaction to the mix

---
+++ consoleio.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+//Do a little console io and printing
+
+
+
+int main(){
+	
+	int a,b,c,sum;
+	char fname[20];
+	char lname[20];
+
+	printf("Please enter in 3 integers to be added\n");
+	scanf("%d%d%d",&a,&b,&c);
+	printf("Please enter you name: Firstname Lastname\n");
+	scanf("%s%s",fname,lname);
+	sum = a + b + c;
+	printf("Hello %s %s. The sum of these integers are: %d \n",fname,lname,sum);
+
+	return 0;
+
+}	
--- fileio.c
+++ fileio.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 //Perform some basic io
 #define MAXLINESIZE 300
@@ -7,9 +8,10 @@ int printFileToConsole(char *fn);
 
 int main(){
 
-	char filename[] = "./text-data-files/rqmsgs_t";
+	char textfile[] = "./text-data-files/rqmsgs_t";
+	//char namefile[] = "./text-data-files/census-dist-male-first.txt"
 	
-	printFileToConsole(filename);
+	printFileToConsole(textfile);
 	
 	return 0;	
 }
@@ -23,7 +25,7 @@ int printFileToConsole(char *fn){
 	
 	if(fp == NULL){
 		perror("Error opening file");
-		return -1;
+		exit(-1);
 	}
 
 	while(fgets(line,MAXLINESIZE,fp)){
@@ -46,5 +48,21 @@ When ranned without the printf
 real	0m0.001s
 user	0m0.001s
 sys	0m0.000s
+
+valgrind --tool=memcheck ./a.out
+==2791== Memcheck, a memory error detector
+==2791== Copyright (C) 2002-2012, and GNU GPL'd, by Julian Seward et al.
+==2791== Using Valgrind-3.8.1 and LibVEX; rerun with -h for copyright info
+==2791== Command: ./a.out
+==2791== 
+==2791== 
+==2791== HEAP SUMMARY:
+==2791==     in use at exit: 0 bytes in 0 blocks
+==2791==   total heap usage: 1 allocs, 1 frees, 568 bytes allocated
+==2791== 
+==2791== All heap blocks were freed -- no leaks are possible
+==2791== 
+==2791== For counts of detected and suppressed errors, rerun with: -v
+==2791== ERROR SUMMARY: 0 errors from 0 contexts (suppressed: 6 from 6)
 
 */
