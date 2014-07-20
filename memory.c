3a9295d63f3c1f1dc5629744c4a297484876926b
Kito Joseph <kitojoseph@gmail.com>
2014-07-20 09:08:24 -0400

    added a memory file , work in progress

---
+++ memory.c
@@ -0,0 +1,35 @@
+#include <stdio.h>   //printf
+#include <stdlib.h>  //man stdlib.h
+#include <stdbool.h> //true
+#include <unistd.h>  //sleep
+#include <sys/types.h> //defines pid_t
+
+
+//Use pmap in conjection with running this program to get a better understand of whats going on
+//ex.  pmap
+//Tutorial about the buffers http://wr.informatik.uni-hamburg.de/_media/teaching/wintersemester_2012_2013/epc-1213-brauer-buffer-praesentation.pdf
+//heap -> malloc
+//stack -> function calls,arguments
+//TODO Need to find out if there is a way to print the stack frame
+//TODO see if we can find out if there is buffer overflow
+/*
+	^     heap allocation    |
+	|     stack allocation   |
+	|     static allocation  |
+	|
+    flexibility                  allocation cost
+*/
+//static declarition of memory is quicker than dynamic allocation
+int main(){
+	int i;
+	char string[] = "string that is a string in memory.c";
+	
+	
+	printf("The pid of this program is: %d\n",getpid());
+	printf("The address of i is %p \n",&i);
+	
+	while(true){
+		sleep(1);
+	}
+	return 0;
+}
