b36ae094f35338cca387450696226e5f6acafad3
Kito Joseph <kjoseph@oc7188553436.ibm.com>
2015-12-16 10:26:03 -0500

    Wrote a stub for using pthread

--- threads.c
+++ threads.c
@@ -1,1 +1,14 @@
-This is a test.
+#include <stdio.h>
+#include <semaphore.h>
+
+//source code: http://softpixel.com/~cwright/programming/threads/threads.c.php
+//In order to compile multithreadded application it is necessary to add the following 
+//option to the gcc comand line: "gcc thread.c -lpthread"
+
+int main(){
+
+	printf("Welcome to pthread tutorial\n");
+	
+	return 0;
+
+}
