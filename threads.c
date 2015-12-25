7d35a63a4898378a377aafaeda0834501a3cd2b1
Kito Joseph <kjoseph@oc7188553436.ibm.com>
2015-12-25 08:10:28 -0500

    Added thread tutorial to the repo

--- threads.c
+++ threads.c
@@ -2,8 +2,13 @@
 #include <semaphore.h>
 
 //source code: http://softpixel.com/~cwright/programming/threads/threads.c.php
+
 //In order to compile multithreadded application it is necessary to add the following 
-//option to the gcc comand line: "gcc thread.c -lpthread"
+//option to the gcc command line: "gcc thread.c -lpthread"
+
+//https://computing.llnl.gov/tutorials/pthreads/
+//https://www.cs.cf.ac.uk/Dave/C/node32.html
+//https://www.cs.cf.ac.uk/Dave/C/node29.html
 
 int main(){
 
