5493d7e3e4136cad50b4401c820b8b3bb165eaee
Kito Joseph <kitojoseph@gmail.com>
2017-01-26 18:35:26 -0500

    made some corrections

--- applicationstub.c
+++ applicationstub.c
@@ -93,25 +93,5 @@ const char* get_process_name_by_pid(pid_t pid)
     return name;
 }
 
-/*
-const char* get_process_name_by_pid(const int pid)
-{
-    char* name = (char*)calloc(1024,sizeof(char));
-    if(name){
-        sprintf(name, "/proc/%d/cmdline",pid);
-        FILE* f = fopen(name,"r");
-        if(f){
-            size_t size;
-            size = fread(name, sizeof(char), 1024, f);
-            if(size>0){
-                if('\n'==name[size-1])
-                    name[size-1]='\0';
-            }
-            fclose(f);
-        }
-    }
-    return name;
-}
-*/
 
 
