3932dce12274eec6ad5bf9d8708b8970eea975d6
Kito Joseph <kitojoseph@gmail.com>
2017-01-26 18:28:39 -0500

    Added how to get the process id by name to the mix

--- applicationstub.c
+++ applicationstub.c
@@ -55,7 +55,7 @@ void PrintApplicationHeader(){
     printf("is to build a solid framework \nfor the application development ");
     printf("process.\n");
     printf("----------------------------------------------------------------\n");
-    printf("The process name of this process is: kjkjkjkjkjkj \n");
+    printf("The process name of this process is: %s \n",get_process_name_by_pid(processid);
 }
 
 void PrintLogFile(FILE *f, char *string){
@@ -73,7 +73,6 @@ void SigtermHandler(int signal, siginfo_t *info, void *_unused)
 }
 
 
-/*
 const char* get_process_name_by_pid(const int pid)
 {
     char* name = (char*)calloc(1024,sizeof(char));
@@ -93,5 +92,5 @@ const char* get_process_name_by_pid(const int pid)
     return name;
 }
 
-*/
+
 
