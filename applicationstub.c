5c70c89b5374ef4181ed2cbf0f81bee31203205c
Kito Joseph <kitojoseph@gmail.com>
2017-02-22 10:07:45 -0500

    Added prolog

--- applicationstub.c
+++ applicationstub.c
@@ -1,3 +1,41 @@
+/****************************************************************
+*FILE NAME:
+*https://github.com/otikkito/cWorld/blob/master/nasa-c-style.pdf
+*PURPOSE
+*
+*FILE REFERENCES:
+*
+*NAME		I/O		Description
+*----           ---             -----------
+*
+*EXTERNAL VARIABLE:
+*SOURCE: <         >
+*
+*NAME	  TYPE    	I/O		Description
+*----     ----          ---             -----------
+*
+*EXTERNAL REFERENCES:
+*
+*Name		Description
+*----   	-----------
+*
+*ABNORMAL ERMINATION CONDITIONS, ERROR AND WARINGING MESSAGES:
+*
+*ASSUMPTIONS, CONSTRAINTS, RESTRICTIONS
+*
+*NOTES:
+*
+*REQUIREMENTS/FUNCTIONAL SPECIFICATIONS REFERENCES:
+*
+*DEVELOPMENT HISTORY:
+*
+*Date	Author	Change ID	Release		Description of Change
+*----   ------  ---------       -------         ---------------------
+*
+*ALGORITHM (PDL)
+*/
+
+
 #include <stdio.h>
 #include <time.h>
 #include <signal.h>
