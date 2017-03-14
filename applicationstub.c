dda234e44cc41731c3f02e16a90832c761de7ca7
Kito Joseph <kitojoseph@gmail.com>
2017-03-14 17:55:26 -0400

    Added an additional way to get the process name none programatically

--- applicationstub.c
+++ applicationstub.c
@@ -127,7 +127,7 @@ void SigtermHandler(int signal, siginfo_t *info, void *_unused)
 }
 
 /*********************************************************************/
-
+/* can also be done be running ps -p PID -i comm= */
 const char* get_process_name_by_pid(pid_t pid)
 {
     char* name = (char*)calloc(1024,sizeof(char));
