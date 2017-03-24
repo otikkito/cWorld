011e78e04dd75bd35aeaed22d32ec4c37950bb1f
Kito Joseph <otikkito@localhost.localdomain>
2017-03-24 11:18:06 -0400

    Added a random number generator that generates a random number with a range

---
+++ rand.c
@@ -0,0 +1,39 @@
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+/*
+ * http://stackoverflow.com/questions/2509679/how-to-generate-a-random-number-from-within-a-range
+ */
+
+
+//function declarations
+long random_at_most(long max);
+
+int main(){
+    srand(time(NULL));   // should only be called once
+    int r = rand();      // returns a pseudo-random integer between 0 and RAND_MAX
+    printf("The value of RAND_MAX = %d and the random number generated is: %d\n",RAND_MAX, r);
+    printf("The value of the random integer in the range of 0 and 30 is: %ld\n",random_at_most(30));
+    
+}
+
+// Assumes 0 <= max <= RAND_MAX
+// Returns in the closed interval [0, max]
+long random_at_most(long max) {
+  unsigned long
+    // max <= RAND_MAX < ULONG_MAX, so this is okay.
+    num_bins = (unsigned long) max + 1,
+    num_rand = (unsigned long) RAND_MAX + 1,
+    bin_size = num_rand / num_bins,
+    defect   = num_rand % num_bins;
+
+  long x;
+  do {
+   x = random();
+  }
+  // This is carefully written not to overflow
+  while (num_rand - defect <= (unsigned long)x);
+
+  // Truncated division is intentional
+  return x/bin_size;
+}
