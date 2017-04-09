794a57f4f7cee0f0ef1a2e59964686305bdae0fa
Kito Joseph <otikkito@unknownf40669a4d0e9.attlocal.net>
2017-04-09 10:23:09 -0400

    Added a link for keywords

---
+++ Docs/cManPages/clanguage.c
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   clanguage.c
+ * Author: otikkito
+ * www.kjoseph-it.com
+ * Created on April 9, 2017, 9:46 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+    
+
+    return (EXIT_SUCCESS);
+}
+
