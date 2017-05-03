a68d39338f1b7ac0a91b0f29bf83ff7ea8b72311
Kito Joseph <kitojoseph@gmail.com>
2017-05-03 19:04:09 -0400

    Modified logger to print the pid to the log. In the future we will print the process name

--- logger.c
+++ logger.c
@@ -3,6 +3,7 @@
 #include <syslog.h>
 #include <stdarg.h>
 #include <sys/types.h>
+#include <unistd.h>
 
 //www.kjoseph-it.com
 
@@ -26,10 +27,10 @@
 /*function prototype*/
 int print_log(char *string);
 int print_syslog(char *string);
-int print_logfile(FILE *f, char *string);
-int print_logfile_va(FILE *f, ...);//proof of concept
-int print_logfile_s(FILE *f, char *string,...);//proof of concept
-int print_logfile_pid(FILE *f, char *string, pid_t);
+int print_logfile(FILE *fp, char *string);
+int print_logfile_va(FILE *fp, ...);//proof of concept
+int print_logfile_s(FILE *fp, char *string,...);//proof of concept
+int print_logfile_pid(FILE *fp, char *string, pid_t pid);
 
 /*Global Variables*/
 FILE *fp;
@@ -48,6 +49,8 @@ int main(){
 	print_logfile(fp,"And we printing to the log file");
 	print_log("Error you machine is going down");
 	print_syslog("Holy moly you got another error");
+        print_logfile_pid(fp,"The pid of this process is: ",getpid());
+        
 	fclose(fp);
 	return 0;
 
@@ -76,8 +79,12 @@ int print_logfile(FILE *f, char *string){
 	return 0;
 }
 
-int print_logfile_pid(FILE *f, char *string, pid_t){
-    
+int print_logfile_pid(FILE *fp, char *string, pid_t pid){
+        char timestring[100];
+	time_t currenttime = time(0);
+	strftime(timestring,sizeof(timestring),"%c",localtime(&currenttime));
+	fprintf(fp,"%s %s %d\n",timestring,string,pid);
+    return 0;   
 }
 
 /*
