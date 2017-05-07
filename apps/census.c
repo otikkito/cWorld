fdb0364a35cf608352eafe2abed3ce3fdec7dfb9
Kito Joseph <kitojoseph@gmail.com>
2017-05-07 16:07:40 -0400

    Added reading of the file now I need to find out how to read it and pack it into a structure

--- apps/census.c
+++ apps/census.c
@@ -9,6 +9,8 @@
  * Author: otikkito
  *
  * Created on May 6, 2017, 5:31 PM
+ * 
+ * https://docs.python.org/2/library/os.html  ->converting file to a easy format
  */
 
 #include <stdio.h>
@@ -17,6 +19,10 @@
 #include <errno.h>
 
 #define MAXLINESIZE 1000
+
+/*Global variables*/
+
+
 /*
  * 
  */
@@ -25,18 +31,19 @@ int main(int argc, char** argv) {
     FILE *fp;
     char line[MAXLINESIZE];
     char cwd[1024];
-   if (getcwd(cwd, sizeof(cwd)) != NULL)
-       fprintf(stdout, "Current working dir: %s\n", cwd);
-   else
+    if (getcwd(cwd, sizeof(cwd)) != NULL){
+        fprintf(stdout, "Current working dir: %s\n", cwd);
+    }
+    else
        perror("getcwd() error");
+    
     fp=fopen("../text-data-files/census-dist-male-first.txt","r");
     if (fp == NULL){
         perror("Unable to open:../text-data-files/census-dis-male-first.txt");
         exit(EXIT_FAILURE);
     }
-    while(fp != EOF){
-        fgets(line,MAXLINESIZE,fp);
-        fputs(line,stdout);
+    while(fgets(line,MAXLINESIZE,fp)){
+        printf("%s",line);
     }
     fclose(fp);
     return (EXIT_SUCCESS);
---
+++ apps/census.h
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   census.h
+ * Author: otikkito
+ *
+ * Created on May 7, 2017, 3:50 PM
+ * 
+ * http://stackoverflow.com/questions/11280523/c-reading-from-file-into-structure
+ */
+
+#ifndef CENSUS_H
+#define CENSUS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* CENSUS_H */
+//1219
+struct datastruct{
+
+	char name[50];
+	float df1;
+        float df2;
+};
