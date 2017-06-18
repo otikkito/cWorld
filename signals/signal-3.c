1c7acb92dc3cabf41215e2a438ff9099de6b32ec
Kito Joseph <kitojoseph@gmail.com>
2017-06-18 09:16:53 -0400

    Did some more testing of signals

--- signals/signal-3.c
+++ signals/signal-3.c
@@ -14,14 +14,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
+#include <errno.h>
+#include <unistd.h>
 
 /*
  * http://www.csl.mtu.edu/cs4411.ck/www/NOTES/signal/two-signals.html
+ * man sigaction
  */
-int main(int argc, char** argv) {
-    
-    
 
+//Function declarations
+void signal_handler(int sig);
+
+
+int main(int argc, char** argv) {
+    struct sigaction sa;
+    
+    sa.__sigaction_handler =signal_handler;
+    sa.sa_flags =   0;
+    sigemptyset(sa.sa_mask);
+    
+    if(sigaction(SIGINT,&sa, NULL) = -1){
+        perror("There is a problem with sigaction");
+        return (EXIT_FAILURE);
+    }
+    
+    sleep(100);
     return (EXIT_SUCCESS);
 }
 
+void signal_handler(int sig){
+    printf("We recieved a signal and will be handle exiting stuff");
+}
