a5d26f9ff55e4aaa1613053ecfe8c802bc6690eb
Kito Joseph <kitojoseph@gmail.com>
2017-06-29 08:34:28 -0400

    Added additional statistics to the timer file

--- timing/timer-good.c
+++ timing/timer-good.c
@@ -154,3 +154,34 @@ inline void diff_timespec(struct timespec *start,struct timespec *end){
     t.nanoseconds = ((end->tv_nsec)-(start->tv_nsec));
     
 }
+
+/*
+ /usr/bin/time -v ./a.out 
+The size of time_t is : 8 
+It took a total of 0 sec. and 448293 nanosecs. 
+Using the new function the total seconds is 0 sec and 448293 nanosecs. 
+	Command being timed: "./a.out"
+	User time (seconds): 0.00
+	System time (seconds): 0.00
+	Percent of CPU this job got: 0%
+	Elapsed (wall clock) time (h:mm:ss or m:ss): 0:00.00
+	Average shared text size (kbytes): 0
+	Average unshared data size (kbytes): 0
+	Average stack size (kbytes): 0
+	Average total size (kbytes): 0
+	Maximum resident set size (kbytes): 480
+	Average resident set size (kbytes): 0
+	Major (requiring I/O) page faults: 0
+	Minor (reclaiming a frame) page faults: 161
+	Voluntary context switches: 2
+	Involuntary context switches: 7
+	Swaps: 0
+	File system inputs: 0
+	File system outputs: 0
+	Socket messages sent: 0
+	Socket messages received: 0
+	Signals delivered: 0
+	Page size (bytes): 4096
+	Exit status: 0
+
+ */
