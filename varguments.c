c9a6b506430df27359a19a839f7b37808ff96bb0
Kito Joseph <kitojoseph@gmail.com>
2017-06-30 12:19:42 -0400

    Moved around some of the files the proper direcotry

--- varguments.c
+++ varguments.c
@@ -3,11 +3,21 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+#include <stdlib.h>
 #include <stdarg.h>
+#include <stdio.h>
+
+/*Funciton prototypes*/
+int FunctionA(int arg_count,...);
 
 /*man stdarg.h*/
 int main(){
     
+    FunctionA("Hello world");
+    return EXIT_SUCCESS;
+}
+
+int FunctionA(int arg_count,...){
+    printf("String: %s \n",string);
     return 0;
 }
