45b02812cfd26049ac212c6a5c1e7c5c63070291
Kito Joseph <kitojoseph@gmail.com>
2017-06-30 15:34:45 -0400

    Corrected a typo

--- memory.c
+++ memory.c
@@ -8,7 +8,7 @@
 //Good tutorial on understanding of process memory --> 
 //https://techtalk.intersec.com/2013/07/memory-part-1-memory-types/
 //https://techtalk.intersec.com/2013/07/memory-part-2-understanding-process-memory/
-//Use pmap in conjection with running this program to get a better understand of whats going on
+//Use pmap in conjuction with running this program to get a better understand of whats going on
 //ex.  pmap
 //Tutorial about the buffers http://wr.informatik.uni-hamburg.de/_media/teaching/wintersemester_2012_2013/epc-1213-brauer-buffer-praesentation.pdf
 //heap -> malloc
