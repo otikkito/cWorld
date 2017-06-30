5b13bc02fdb91b74f830a4721f396e2c7f7c058e
Kito Joseph <kitojoseph@gmail.com>
2017-06-30 19:18:06 -0400

    Added a glade design however the application is not compiling at this time.

--- c-graphics/gtk/gtk-hello-world.c
+++ c-graphics/gtk/gtk-hello-world.c
@@ -15,11 +15,54 @@
 #include <stdlib.h>
 #include <gtk/gtk.h>
 
+
+/*Function prototype*/
+void on_window_main_destroy();
+
+/*Global variables*/
+GtkWidget *g_lbl_hello;
+GtkWidget *g_lbl_count;
+
+
+void on_btn_hello_clicked(){
+    static unsigned int count = 0;
+    char str_count[30] = {0};
+    
+    gtk_label_set_text(GTK_LABEL(g_lbl_hello), "Hello, world!");
+    count++;
+    sprintf(str_count, "%d", count);
+    gtk_label_set_text(GTK_LABEL(g_lbl_count), str_count);
+}
 /*
  * 
  */
 int main(int argc, char** argv) {
+    
+    GtkBuilder      *builder; 
+    GtkWidget       *window;
+ 
+    gtk_init(&argc, &argv);
+ 
+    builder = gtk_builder_new();
+    gtk_builder_add_from_file (builder, "/home/otikkito/myCode/glade-gui/gtk-hello-world.glade", NULL);
+ 
+    window = GTK_WIDGET(gtk_builder_get_object(builder, "window_main"));
+    gtk_builder_connect_signals(builder, NULL);
+    
+    g_lbl_hello = GTK_WIDGET(gtk_builder_get_object(builder, "lbl_hello"));
+    g_lbl_count = GTK_WIDGET(gtk_builder_get_object(builder, "lbl_count"));
 
+    g_object_unref(builder); //https://bugzilla.redhat.com/show_bug.cgi?id=712225
+ 
+    gtk_widget_show(window);                
+    gtk_main();
+ 
+    
     return (EXIT_SUCCESS);
 }
 
+// called when window is closed
+void on_window_main_destroy()
+{
+    gtk_main_quit();
+}
