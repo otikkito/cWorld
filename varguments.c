e386793c9497dbe40dbb60c849ec791fb553f0dc
Kito Joseph <kitojoseph@gmail.com>
2017-06-30 13:54:23 -0400

    Added some additional lines of code but they are not being displayed in order which needs to be corrected.

--- varguments.c
+++ varguments.c
@@ -7,24 +7,31 @@
 #include <stdarg.h>
 #include <stdio.h>
 
+
 /*Funciton prototypes*/
 int FunctionA(int argument_count,...);
+
+/*Global functions*/
+
 
 /*man stdarg.h*/
 int main(){
     
-    FunctionA(1,"Hello world","It is working");
+    FunctionA(1,"It is working","Hello world", "This is argument 3");
     return EXIT_SUCCESS;
 }
 
 /* This is a variadic function
  * man stdarg.h
- * Also check out vprintf()
+ * Also check out vfprintf() man stdio.h
  */
 int FunctionA(int argument_count,...){
+       
     va_list ap;
     va_start(ap,argument_count);
+    /*TODO I need to fixed the order at which this is displayed*/
+    printf("Count: %d %s %s %s\n",argument_count,va_arg(ap, char *), va_arg(ap,char *), va_arg(ap, char *));
     
-    printf("Count: %d %s %s\n",argument_count,va_arg(ap, char *), va_arg(ap,char *));
+    va_end(ap);
     return 0;
 }
