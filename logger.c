cebd2a66d14e6f8ccefa7bcc6f884f5ed3870164
Kito Joseph <kitojoseph@gmail.com>
2017-06-30 10:58:12 -0400

    Added file and function header

--- logger.c
+++ logger.c
@@ -7,9 +7,9 @@
 
 //www.kjoseph-it.com
 
-//I will basically log a couple of things to the application log 
-//and if this code is executing on Linux we will log stuff to the syslog
-
+/*I will log a couple of things to the application log 
+*and if this code is executing on Linux we will log stuff to the syslog
+*/
 /* TODO need to create a macro of print_log 
  Also could do some measurements as far as which one is quicker
 */
@@ -39,14 +39,11 @@ FILE *fp;
 int main(){
 	
 	char logfile[]= "./text-data-files/logfile.txt";
-	
-	
-	fp = fopen(logfile,"a+");   //Initially caused a segfault because no error checking
-	
+
+	fp = fopen(logfile,"a+");   //Initially caused a segfault because no error checking	
 	if(fp == NULL){
 		perror("Errror with fopen");
 	}
-
 	print_logfile(fp,"And we printing to the log file");
 	print_log("Error you machine is going down");
 	print_syslog("Holy moly you got another error");
@@ -56,6 +53,28 @@ int main(){
 	return 0;
 
 }
+
+/********************************************************
+*
+*
+* FUNCTION NAME:
+*
+*
+*
+* ARGUMENTS:
+*
+*
+*
+* ARGUMENT     TYPE I/O DESCRIPTION
+* --------     ---- --- -----------
+*
+*
+*
+* RETURNS:
+*
+*
+*
+*********************************************************/
 int print_log(char *string){
 
 	char timestring[100];
@@ -66,12 +85,53 @@ int print_log(char *string){
 	return 0;	
 }
 
+/********************************************************
+*
+*
+* FUNCTION NAME:
+*
+*
+*
+* ARGUMENTS:
+*
+*
+*
+* ARGUMENT     TYPE I/O DESCRIPTION
+* --------     ---- --- -----------
+*
+*
+*
+* RETURNS:
+*
+*
+*
+*********************************************************/
 /* More information can be found man 3 syslog */
 int print_syslog(char *string){
 	syslog(LOG_ERR,"Just playing");
 	return 0;
 }
-
+/********************************************************
+*
+*
+* FUNCTION NAME:
+*
+*
+*
+* ARGUMENTS:
+*
+*
+*
+* ARGUMENT     TYPE I/O DESCRIPTION
+* --------     ---- --- -----------
+*
+*
+*
+* RETURNS:
+*
+*
+*
+*********************************************************/
 int print_logfile(FILE *f, char *string){
 	char timestring[100];
 	time_t currenttime = time(0);
@@ -80,6 +140,27 @@ int print_logfile(FILE *f, char *string){
 	return 0;
 }
 
+/********************************************************
+*
+*
+* FUNCTION NAME:
+*
+*
+*
+* ARGUMENTS:
+*
+*
+*
+* ARGUMENT     TYPE I/O DESCRIPTION
+* --------     ---- --- -----------
+*
+*
+*
+* RETURNS:
+*
+*
+*
+*********************************************************/
 int print_logfile_pid(FILE *fp, char *string, pid_t pid){
         char timestring[100];
 	time_t currenttime = time(0);
@@ -88,6 +169,27 @@ int print_logfile_pid(FILE *fp, char *string, pid_t pid){
     return 0;   
 }
 
+/********************************************************
+*
+*
+* FUNCTION NAME:
+*
+*
+*
+* ARGUMENTS:
+*
+*
+*
+* ARGUMENT     TYPE I/O DESCRIPTION
+* --------     ---- --- -----------
+*
+*
+*
+* RETURNS:
+*
+*
+*
+*********************************************************/
 /*
  * Doing this will make it non portable or a good idea another way is to try with sprintf
  * int sprintf(char *str, const char *format, ...);
