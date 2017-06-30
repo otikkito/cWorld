aa2a634ee32d1bd03e29436959dc0d75ce4201b7
Kito Joseph <kitojoseph@gmail.com>
2017-06-30 16:19:59 -0400

    Added some more code to the test1.c to print in input the word. I need to work on the floats and convert them from strings.

--- apps/test1.c
+++ apps/test1.c
@@ -10,7 +10,7 @@
   https://en.wikipedia.org/wiki/Natural_language_processing
   https://www.coursera.org/learn/natural-language-processing
   http://opennlp.apache.org/
-*/
+ */
 
 /*
  * Some of the issues that I have seen thus far is that to my knowledge there
@@ -21,22 +21,20 @@
 void tokenizestring(char * string);
 int isblank_k(int c);
 
-
-struct strings{
+struct strings {
     char string1[50];
     float float1;
     float float2;
-    int   integer;
-}string;
+    int integer;
+} string;
 
-int main(){
+int main() {
 
-	char string[] = "JAMES          3.318  3.318      1\n";
-        tokenizestring(string);
-        
-	return 0;
+    char string[] = "JAMES          3.318  3.318      1\n";
+    tokenizestring(string);
+
+    return 0;
 }
-
 
 /*
  char string[] = "JAMES          3.318  3.318      1\n";
@@ -47,50 +45,54 @@ int main(){
        a zero value if not.
 
  */
-void tokenizestring(char * string){
-    printf("This is the string: %s",string);
+void tokenizestring(char * string) {
+    printf("This is the string: %s", string);
     //Break up each word and put it into a string
     int i;
-    int membernum=1;
+    int membernum = 1;
     int j;
     int count;
     char c;
     char word[WORDSIZE];
-    
-    memset(&word[0],0, strlen(word));
-    for(i=0; i < strlen(string);i++){  //need to fix strlen it returns size_t
+    char wordf1[WORDSIZE];
+    char wordf2[WORDSIZE];
+
+
+    memset(&word[0], 0, strlen(word));
+    for (i = 0; i < strlen(string); i++) { //need to fix strlen it returns size_t
         c = string[i];
-        if(isblank(string[i]) == 0){  
-            printf("%c \n",string[i]);
-            word[i]= c;
-        }
+        
         //char string[] = "JAMES          3.318  3.318      1\n";
-        if((isblank(string[i])) == 0){//if it is a character print it to the console and put it in one of the member of the structure
-            switch(membernum){
+        if ((isblank(string[i])) == 0) {//if it is a character print it to the console and put it in one of the member of the structure
+            switch (membernum) {
                 case 1:
-                    mebernum++;
-                    continue;
+                    printf("%c \n", string[i]);
+                    word[i] = c;
+                    break;
                 case 2://need to modify to read in a float
                     //man atof
                     ///https://www.tutorialspoint.com/c_standard_library/c_function_atof.htm
                     //string.float1[i] = c;
                     printf("Now on member 2");
-                    continue;
-                
+                    break;
+
                 case 3: //need to modify to read in a float
                     //string.float2[i] = c;
                     break;
                 case 4: //need to  //string.int[i] = c;
                     break;
             }
-            membernum++;
-            
+            if((isblank(c))!=0){
+                membernum++;
+            }
+
         }
-        
+
     }
-    
-    printf("word: %s \n",word);
+
+    printf("word: %s %s %s\n", word,wordf1,wordf2);
 }
+
 /*
  blanks that are in c:
  * man ascii
@@ -98,8 +100,8 @@ void tokenizestring(char * string){
        a zero value if not.
  * if it is blank return 1 and if it is not blank return 0
  */
-int isblank_k(int c){
-    if((c == atoi(" ")) || (c == atoi("\t"))){
+int isblank_k(int c) {
+    if ((c == atoi(" ")) || (c == atoi("\t"))) {
         return 1;
     }
     return 0;
