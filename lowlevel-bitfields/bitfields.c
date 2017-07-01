971b822ca0e7ea3a82a7571346600cf182a385b5
Kito Joseph <kitojoseph@gmail.com>
2017-07-01 16:31:15 -0400

    Removed some of the files to the proper directory.

---
+++ lowlevel-bitfields/bitfields.c
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   bitfields.c
+ * Author: otikkito
+ *
+ * Created on July 1, 2017, 4:27 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*Referance:
+ https://users.cs.cf.ac.uk/Dave.Marshall/C/node13.html
+ */
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
