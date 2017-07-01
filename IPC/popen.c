3a5d17a94df991a7d0fc4db8e02684eee650eb73
Kito Joseph <kitojoseph@gmail.com>
2017-07-01 12:46:32 -0400

    Trying to get popen working but it is stating that there is a broken pipe.

--- IPC/popen.c
+++ IPC/popen.c
@@ -2,7 +2,15 @@
 /*http://pubs.opengroup.org/onlinepubs/009695399/functions/popen.html*/
 
 int main(){
-
-
-	return 0;
+    
+    FILE *fp;
+    
+    fp = popen("cat /etc/redhat-release","r");
+    if(fp == NULL){
+        perror("Problem with popen.");
+        exit(EXIT_FAILURE);
+        /*cat: write error: Broken pipe*/
+    }
+    
+    return 0;
 }
