8a20f8af2e24885b64cd8d2941106b85ca4121f4
Kito Joseph <kitojoseph@gmail.com>
2017-07-01 12:48:18 -0400

    Added a bug reference of possible association.

--- IPC/popen.c
+++ IPC/popen.c
@@ -10,6 +10,9 @@ int main(){
         perror("Problem with popen.");
         exit(EXIT_FAILURE);
         /*cat: write error: Broken pipe*/
+        /*Possible bug. Further investigation is required
+         https://bugzilla.redhat.com/show_bug.cgi?id=1323401
+        */
     }
     
     return 0;
