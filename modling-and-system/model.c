3a77f3b2e7ceb6fc77e56544473495c6aa645eca
Kito Joseph <kitojoseph@gmail.com>
2017-07-01 16:04:09 -0400

    Added system modeling directory.

---
+++ modling-and-system/model.c
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   model.c
+ * Author: otikkito
+ *
+ * Created on July 1, 2017, 3:25 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/*Function prototypes*/
+int input();
+int output();
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
