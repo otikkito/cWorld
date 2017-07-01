9f1cf194c34a22b9a284a37e3a06c2e7038d0d7a
Kito Joseph <kitojoseph@gmail.com>
2017-07-01 17:11:07 -0400

    Added some additional code and need to finish the exercise.

--- lowlevel-bitfields/bitfields.c
+++ lowlevel-bitfields/bitfields.c
@@ -13,6 +13,10 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+
+/*Function prototype*/
+void Ex_12507(unsigned char a);
 
 /*Referance:
  https://users.cs.cf.ac.uk/Dave.Marshall/C/node13.html
@@ -20,8 +24,56 @@
 /*
  * 
  */
+
+
+struct packed_struct {
+		 unsigned int f1:1;
+		 unsigned int f2:1;
+		 unsigned int f3:1;
+		 unsigned int f4:1;
+		 unsigned int type:4;
+		 unsigned int funny_int:9;
+} pack;
+/*"Here the packed_struct contains 6 members: Four 1 bit flags f1..f3, a 4 bit 
+ * type and a 9 bit funny_int. "*/
+
+struct DISK_REGISTER  {
+     unsigned ready:1;
+     unsigned error_occured:1;
+     unsigned disk_spinning:1;
+     unsigned write_protect:1;
+     unsigned head_loaded:1;
+     unsigned error_code:8;
+     unsigned track:9;
+     unsigned sector:5;
+     unsigned command:5;
+};
+
+
 int main(int argc, char** argv) {
 
+    system("cat /etc/redhat-release");
+    pack.f1 = true;
+    unsigned char a;
+    a = 3;
+    printf("The size of unsigned char is: %d \n",sizeof(a));
+    Ex_12507(a);
     return (EXIT_SUCCESS);
 }
 
+void Ex_12507(unsigned char a){
+    
+    struct u8{
+        unsigned int bit1:1;
+        unsigned int bit2:1;
+        unsigned int bit3:1;
+        unsigned int bit4:1;
+        unsigned int bit5:1;
+        unsigned int bit6:1;
+        unsigned int bit7:1;
+        unsigned int bit8:1;
+        
+    };
+    struct u8 temp;
+    temp = a;
+}
