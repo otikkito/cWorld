c057eae0e6355f1d2c8afeb67dec46bd0689fae8
Kito Joseph <kitojoseph@gmail.com>
2017-07-01 12:18:47 -0400

    Added popen to open subprocess.

---
+++ IPC/popen.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+/*http://pubs.opengroup.org/onlinepubs/009695399/functions/popen.html*/
+
+int main(){
+
+
+	return 0;
+}
--- macro-preprocessor.c
+++ macro-preprocessor.c
@@ -11,16 +11,32 @@
  * Created on July 1, 2017, 9:57 AM
  */
 
-/* Reference: https://www-s.acm.illinois.edu/webmonkeys/book/c_guide/1.7.html */
-
+/* Reference: 
+ * https://www-s.acm.illinois.edu/webmonkeys/book/c_guide/1.7.html 
+ * http://en.cppreference.com/w/c/language/constant_expression
+ * https://gcc.gnu.org/onlinedocs/cpp/Standard-Predefined-Macros.html
+ * http://northstar-www.dartmouth.edu/doc/ibmcxx/en_US/doc/language/concepts/cuexpcon.htm
+ */
+/*Notes:
+ * constant_expression: A constant expression is an expression with a value that 
+ * is determined during compilation. That value can be evaluated at runtime, but
+ * cannot be changed. Constant expressions can be composed of integer, 
+ * character, floating-point, and enumeration constants, as well as other 
+ * constant expressions.
+ */
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
 
 /*
  * 
  */
-int main(int argc, char** argv) {
-
+int main(int argc, char** argv){
+    
+    FILE *fp;
+ /* #line 37 */
+    printf("Working with macros and preprocessor directives %s %d\n",__FILE__, __LINE__);
+    
     return (EXIT_SUCCESS);
 }
 
