e4210929c0de636e014c7741e08b4fd4b61bcf7b
Kito Joseph <kitojoseph@gmail.com>
2017-07-08 16:58:38 -0400

    Fixed a typo.

--- varguments.c
+++ varguments.c
@@ -60,7 +60,7 @@ int FunctionA(int argument_count,...){
 
 /* https://stackoverflow.com/questions/14358497/what-is-the-difference-between-fprintf-and-vfprintf-in-c*/
 /*int vfprintf(FILE *stream, const char *format, va_list ap);*/
-/* man vfprintf*/
+/* man vfprintf */
 void print_log_va(FILE *fp,int argcount,...){
     va_list ap;
     va_start(ap,argcount);
