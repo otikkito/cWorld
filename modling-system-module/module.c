7ca92da6b208c9700176ba4c9818876b3a853e7f
Kito Joseph <kitojoseph@gmail.com>
2017-07-15 17:36:54 -0400

    Added some notes on module design

--- modling-system-module/module.c
+++ modling-system-module/module.c
@@ -1,1 +1,13 @@
 //http://www.embedded.com/design/prototyping-and-development/4023876/Modular-Programming-in-C
+
+//Preprocessor directives
+
+
+//module interface
+
+//module implementation
+
+
+"Modules provide abstraction, encapsulation, and information-hiding, making the
+large-scale structure of a program easier to understand. Careful design of 
+modules also promotes software reuse, even in embedded systems programming." 
