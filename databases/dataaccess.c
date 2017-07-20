442dcb77bbe3480db2aa531d7421c3f09ee611c7
Kito Joseph <kitojoseph@gmail.com>
2017-07-20 19:40:28 -0400

    Added dataacces.c to the repository.

---
+++ databases/dataaccess.c
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   dataaccess.c
+ * Author: otikkito
+ *
+ * Created on July 20, 2017, 6:49 PM
+ */
+
+/*http://zetcode.com/db/mysqlc/*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <mysql/my_global.h>
+#include <mysql/mysql.h>
+#include <mysql/my_config.h>
+/*In file included from dataaccess.c:19:0:                                                                          
+/usr/include/mysql/my_global.h:79:23: fatal error: my_config.h: No such file or directory                         
+ #include <my_config.h>
+*/
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+    
+
+    return (EXIT_SUCCESS);
+}
+
