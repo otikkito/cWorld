30b48817030799565dab5a668c986ebc30869293
Kito Joseph <kitojoseph@gmail.com>
2017-07-21 13:46:40 -0400

    I was able to get the database to work and retrive data from the fields.

--- databases/dataaccess.c
+++ databases/dataaccess.c
@@ -15,27 +15,71 @@
 
 /*To compile: 
  * gcc -g -Wall -pedantic dataaccess.c -L/usr/lib64/mysql -lmysqlclient -lpthread -lz -lm -lssl -lcrypto -ldl  -I/usr/include/mysql
-*/
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <mysql/my_global.h>
 #include <mysql/mysql.h>
 #include <mysql/my_config.h>
+
 /*In file included from dataaccess.c:19:0:                                                                          
 /usr/include/mysql/my_global.h:79:23: fatal error: my_config.h: No such file or directory                         
  #include <my_config.h>
  * 
  * gcc -g -Wall -pedantic dataaccess.c -I/usr/include/mysql
 
-*/
+ */
+
+void finish_with_error(MYSQL *con) {
+    fprintf(stderr, "%s\n", mysql_error(con));
+    mysql_close(con);
+    exit(1);
+}
+
 /*
  * 
  */
 int main(int argc, char** argv) {
+
+    printf("MySql client version: %s\n", mysql_get_client_info());
+
+    MYSQL *con = mysql_init(NULL);
+
+    if (con == NULL) {
+        fprintf(stderr, "mysql_init() failed\n");
+        exit(1);
+    }
+
+    if (mysql_real_connect(con, "localhost", "root", "redhat",
+            "census_database", 0, NULL, 0) == NULL) {
+        finish_with_error(con);
+    }
+
+    if (mysql_query(con, "SELECT * FROM census")) {
+        finish_with_error(con);
+    }
+
+    MYSQL_RES *result = mysql_store_result(con);
+
+    if (result == NULL) {
+        finish_with_error(con);
+    }
+
+    int num_fields = mysql_num_fields(result);
+
+    MYSQL_ROW row;
+    int i;
     
-    printf("MySql client version: %s\n",mysql_get_client_info());
-    
+    while ((row = mysql_fetch_row(result))) {
+        for (i = 0; i < num_fields; i++) {
+            printf("%s ", row[i] ? row[i] : "NULL");
+        }
+        printf("\n");
+    }
+
+    mysql_free_result(result);
+    mysql_close(con);
 
     return (EXIT_SUCCESS);
 }
