c773a595088d1d18ffb69c8ab0553326c38ac7ba
Kito Joseph <kitojoseph@gmail.com>
2017-07-23 15:00:58 -0400

    Added a treads to a file but is not working as of yet.

--- threads/threads.c
+++ threads/threads.c
@@ -1,14 +1,31 @@
 #include <stdio.h>
 #include <pthread.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 /* man 7 pthreads */
 /* insure that functions are thread say and proper synchronization */
 
+/*Function prototype*/
+void *thread_run();
 
 int main(){
-
-	printf("Welcome to pthread tutorial\n");
+        
+    int i;
+    pthread_t thread1;
+    
+    printf("Welcome to pthread tutorial\n");
+    
+    i = pthread_create(&thread1,NULL,thread_run,NULL); 
+    if(i !=0){
+        perror("There was a problem with pthread_create");
+    }
 	
-	return 0;
+    return 0;
 
 }
+
+void *thread_run(){
+    sleep(3);
+    pthread_exit(NULL);
+}   
