cffc44958f165160ae0c4f6f224f5a571d5c5219
Kito Joseph <kitojoseph@gmail.com>
2017-07-28 15:10:53 -0400

    Added the stub for the Stock Selector.

---
+++ c++/StockSelector/StockSelector-Main.cpp
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   StockSelector-Main.cpp
+ * Author: otikkito
+ *
+ * Created on July 28, 2017, 2:39 PM
+ */
+
+/*Preprocessor includes*/
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+/*Name space*/
+using namespace std;
+
+/*Class declaration*/
+class StockSelector{
+    string ticker_symbol;
+    
+public:
+    void update_ticker_symbol(string ticker);
+    void display_stock();
+};
+
+/*Class functions*/
+
+void StockSelector::update_ticker_symbol(string ticker){
+    ticker_symbol = ticker;
+    
+}
+
+void StockSelector::display_stock(){
+    cout << ticker_symbol << endl;
+}
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    StockSelector s;
+    s.update_ticker_symbol("rht");
+    s.display_stock();
+    return 0;
+}
+
