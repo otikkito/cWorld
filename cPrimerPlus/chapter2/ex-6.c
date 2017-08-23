7b7a2b7c769c503c85399cc6d314440045670e95
Kito Joseph <kitojoseph@gmail.com>
2017-08-23 18:53:09 -0400

    Added some exercieses to c primer plus.

---
+++ cPrimerPlus/chapter2/ex-6.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+	int toes = 10;
+	int temp;
+	printf("Initially toes is: %d\n",toes);
+	temp = toes * 2;
+	printf("Twice toes is: %d\n",temp);
+	temp = toes * toes;
+	printf("Toes squared is : %d\n",temp);
+	
+}
---
+++ cPrimerPlus/chapter2/ex-7.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+void smile();
+
+int main(){
+	smile();
+	smile();
+	smile();
+	printf("\n");
+	smile();
+	smile();
+	printf("\n");
+	smile();
+	
+}
+
+void smile(){
+	printf("Smile!");
+}
+	
+	
