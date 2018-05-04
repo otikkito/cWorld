ea83706eeff0c4428e2bcba413974052309ba8ad
Kito Joseph <kitojoseph@gmail.com>
2018-05-04 10:19:53 -0400

    Corrected some notes and added stub for the test..

--- applicationstubtest.c
+++ applicationstubtest.c
@@ -2,12 +2,17 @@
 #include <signal.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include "modling-system-module/modules/system-function.h"
 
+//function declaration
+//pid_t get_pid_by_process_name(const char*) //located in : modling-system-module/modules/system-function.h
 
 int main(){
     
-    printf("The process of pid is %u",getpid());
-    kill(5432,SIGINT);
+    printf("The process of pid is %u",getpid()); //TODO correct this
+    
+    
+    kill(get_pid_by_process_name("applicationstub",SIGINT);
     
     return EXIT_SUCCESS;
 }
