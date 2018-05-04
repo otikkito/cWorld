8ab0738ae43c00952a153b121df334103127a674
Kito Joseph <kitojoseph@gmail.com>
2018-05-04 10:33:03 -0400

    Added some console notes.
    Added some note to explain what the test harness is doing.

--- applicationstubtest.c
+++ applicationstubtest.c
@@ -9,10 +9,16 @@
 
 int main(){
     
-    printf("The process of pid is %u",getpid()); //TODO correct this
+    printf("Welcome tot the application-stub Test harness\n"); 
+    printf("The process id of the test harness is %u\n",getpid());
+    printf("-------------------------------------------------");
     
     
     kill(get_pid_by_process_name("applicationstub",SIGINT);
     
+    printf("Thanks for using the application-stub Test harness.\n
+            We are now closing down.");
+    printf("-------------------------------------------------");
+         
     return EXIT_SUCCESS;
 }
