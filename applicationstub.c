137f521ddbf2dbb574f17e24fe7c393af5dc82cb
Kito Joseph <kitojoseph@gmail.com>
2018-08-05 12:45:22 -0400

    Added all the signals that nead to be handle. TODO: add test for each one.

--- applicationstub.c
+++ applicationstub.c
@@ -244,7 +244,7 @@ void intialize_signal_handles(){
 	sigaction(SIGPIPE, &action, NULL);
 	sigaction(SIGALRM, &action, NULL);
 	sigaction(SIGTERM,&action, NULL);
-	sigaction(SIGSTKFLT, &action, NULL);  //This gives a compiler error and I need to chross check with Red Hat documentation.
+	sigaction(SIGSTKFLT, &action, NULL);  //This gives a compiler error and I need to cross check with Red Hat documentation.
 	sigaction(SIGCHLD, &action, NULL);
 	sigaction(SIGCONT,&action, NULL);
 	sigaction(SIGSTOP, &action, NULL);
@@ -260,9 +260,36 @@ void intialize_signal_handles(){
 	sigaction(SIGIO, &action, NULL); //29
 	sigaction(SIGPWR, &action, NULL);
 	sigaction(SIGSYS, &action, NULL);
-	
-	
-    
+	sigaction(SIGRTMIN, &action, NULL);
+	sigaction(SIGRTMIN+1, &action, NULL);
+	sigaction(SIGRTMIN+2, &action, NULL);
+	sigaction(SIGRTMIN+3, &action, NULL);
+	sigaction(SIGRTMIN+4, &action, NULL);
+	sigaction(SIGRTMIN+5, &action, NULL);
+	sigaction(SIGRTMIN+6, &action, NULL);
+	sigaction(SIGRTMIN+7, &action, NULL);
+	sigaction(SIGRTMIN+8, &action, NULL);
+	sigaction(SIGRTMIN+9, &action, NULL);
+	sigaction(SIGRTMIN+10, &action, NULL);
+	sigaction(SIGRTMIN+11, &action, NULL);
+	sigaction(SIGRTMIN+12, &action, NULL);
+	sigaction(SIGRTMIN+13, &action, NULL);
+	sigaction(SIGRTMIN+14, &action, NULL);
+    sigaction(SIGRTMIN+15, &action, NULL);
+	sigaction(SIGRTMAX-14, &action, NULL);
+	sigaction(SIGRTMAX-12, &action, NULL);
+	sigaction(SIGRTMAX-11, &action, NULL);
+	sigaction(SIGRTMAX-10, &action, NULL);
+	sigaction(SIGRTMAX-9, &action, NULL);
+	sigaction(SIGRTMAX-8, &action, NULL);
+	sigaction(SIGRTMAX-7, &action, NULL);
+	sigaction(SIGRTMAX-6, &action, NULL);
+	sigaction(SIGRTMAX-5, &action, NULL);
+	sigaction(SIGRTMAX-4, &action, NULL);
+	sigaction(SIGRTMAX-3, &action, NULL);
+	sigaction(SIGRTMAX-2, &action, NULL);
+	sigaction(SIGRTMAX-1, &action, NULL);
+	sigaction(SIGRTMAX, &action, NULL);
 	/*
 	 1) SIGHUP	 2) SIGINT	 3) SIGQUIT	 4) SIGILL	 5) SIGTRAP
  6) SIGABRT	 7) SIGBUS	 8) SIGFPE	 9) SIGKILL	10) SIGUSR1
@@ -270,7 +297,7 @@ void intialize_signal_handles(){
 16) SIGSTKFLT	17) SIGCHLD	18) SIGCONT	19) SIGSTOP	20) SIGTSTP
 21) SIGTTIN	22) SIGTTOU	23) SIGURG	24) SIGXCPU	25) SIGXFSZ
 26) SIGVTALRM	27) SIGPROF	28) SIGWINCH	29) SIGIO	30) SIGPWR
-31) SIGSYS	34) SIGRTMIN	35) SIGRTMIN+1	36) SIGRTMIN+2	37) SIGRTMIN+3
+31) SIGSYS	34)- SIGRTMIN	35) SIGRTMIN+1	36) SIGRTMIN+2	37) SIGRTMIN+3
 38) SIGRTMIN+4	39) SIGRTMIN+5	40) SIGRTMIN+6	41) SIGRTMIN+7	42) SIGRTMIN+8
 43) SIGRTMIN+9	44) SIGRTMIN+10	45) SIGRTMIN+11	46) SIGRTMIN+12	47) SIGRTMIN+13
 48) SIGRTMIN+14	49) SIGRTMIN+15	50) SIGRTMAX-14	51) SIGRTMAX-13	52) SIGRTMAX-12
