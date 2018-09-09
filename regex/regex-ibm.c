f8498b36cacb89ad43c380e779148c1b899c40f3
Kito Joseph <kitojoseph@gmail.com>
2018-09-09 18:35:09 -0400

    Moved the regular expression to the regex directory and added some more notes.

--- regex/regex-ibm.c
+++ regex/regex-ibm.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*
+/* To find out more "man regcomp"
 int regcomp(regex_t *restrict, const char *restrict, int);
 size_t regerror(int, const regex_t *restrict, char *restrict, size_t);
 int regexec(const regex_t *restrict, const char *restrict, size_t,
@@ -24,25 +24,29 @@ int main(){
 *preg           regex_t   O 
 *pattern        char *    I
 *REG_EXTENDED   int?      I   cflag parameter to the functinos that are symobolic constants
-*returns:
+*
+*returns: int,  the functino regcomp shall return 0 upon succesfull completion.
+*The regcomp() function shall compile the regular expression contained in the string pointed to by the pattern
+*argument and place the results in the structure pointed to by preg. The cflags argument is the bitwiseinclusive
+*OR of zero or more of the following flags, which are defined in the <regex.h> header:
 */
 	if((rc = regcomp(&preg,pattern, REG_EXTENDED)) != 0){
 		printf("regcom() failed, returning nonzero (%d)\n",rc);
 		exit(1);
 	}
-
+	printf("The return code of regcomp() is : %d\n",rc);
 	/*	
 * ARGUMENT     TYPE      I/O DESCRIPTION
 * --------     ----      --- -----------
 *preg          regex_t
-*string
-*nmatch
-*pmatch
-*returns:
+*string        char *
+*nmatch        size_t
+*pmatch        regmatch_t
+*returns: int, the functino regexec shall return 0 upon succesfull completion.
 */
 	if((rc = regexec(&preg, string, nmatch,pmatch,0)) != 0){
 		printf("failed to ERE match '%s' with '%s', returning %d.\n", string, pattern, rc);
 	}
-	
+	printf("The return code of regexec() is : %d\n",rc);
 	regfree(&preg);
 }
