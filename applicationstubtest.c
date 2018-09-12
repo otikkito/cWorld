ae7f6ac4bce1217c3c8b6f879609740f4e4e7803
Kito Joseph <kitojoseph@gmail.com>
2018-09-12 19:08:02 -0400

    Added file format.

--- applicationstubtest.c
+++ applicationstubtest.c
@@ -1,3 +1,43 @@
+/****************************************************************
+*FILE NAME: applicationstubtest.c
+*
+*
+*
+*PURPOSE: Robost test framework for the applicationstub.c
+* 
+*
+*FILE REFERENCES:
+*
+*NAME		I/O		Description
+*----           ---             -----------
+*
+*EXTERNAL VARIABLE:
+*SOURCE: <         >
+*
+*NAME	  TYPE    	I/O		Description
+*----     ----          ---             -----------
+*
+*EXTERNAL REFERENCES:
+*
+*Name		Description
+*----   	-----------
+*
+*ABNORMAL ERMINATION CONDITIONS, ERROR AND WARINGING MESSAGES:
+*
+*ASSUMPTIONS, CONSTRAINTS, RESTRICTIONS
+*
+*NOTES:
+*
+*REQUIREMENTS/FUNCTIONAL SPECIFICATIONS REFERENCES:
+*
+*DEVELOPMENT HISTORY:
+*
+*Date		Author		Change ID	Release		Description of Change
+*----   	------  	---------   -------     ---------------------
+*
+*ALGORITHM (PDL)
+*/
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <signal.h>
