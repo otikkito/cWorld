0382fc229dee9eeec7ae3e5fc953cbaf76959435
Kito Joseph <kitojoseph@gmail.com>
2018-11-16 07:39:12 -0500

    Corrected a typo.

--- applicationstub.c
+++ applicationstub.c
@@ -193,7 +193,7 @@ int main(int argc, char** argv) {
 /********************************************************
  *
  *
- * FUNCTION NAME: printApplicationHeaderToC`onsole
+ * FUNCTION NAME: printApplicationHeaderToConsole
  *
  *
  *
