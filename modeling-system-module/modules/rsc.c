41297d936283c12927ab9384e159fc33f3aa5c2b
Kito Joseph <kitojoseph@gmail.com>
2019-11-22 18:13:40 -0500

    Added some notes about the implementation.

--- modeling-system-module/modules/rsc.c
+++ modeling-system-module/modules/rsc.c
@@ -4,12 +4,19 @@
 
 FILE *runSystemCommand(const char *system_comand);
 
+
+/*
+ Some of the problems and design considerations for runSystemCommand
+ 1. ensure that the command is right before trying to running the command
+ 2. I ran into a problem with "ps -aux" and "ps - aux" with cygwin
+ 3. locating substring in string "strstr()" function
+*/
 int main(int argc, char** argv) {
 	FILE *fp;
 	char line[LINE_MAX];
 	
 	printf("Welcome to run system command.\n");
-	fp = runSystemCommand("ps -aux");
+	fp = runSystemCommand("ps - aux");
 	
 	/* char *fgets(char *s, int size, FILE *stream); */
 	while((fgets(line,LINE_MAX,fp)) != NULL){
