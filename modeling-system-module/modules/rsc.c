182483552baf4b122746ae0e4efb4be98aa0f851
Kito Joseph <kitojoseph@gmail.com>
2019-11-22 20:03:38 -0500

    Another incrementl comment that includes the return code when you close the process. TODO still need to clean up and simply.

--- modeling-system-module/modules/rsc.c
+++ modeling-system-module/modules/rsc.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <limits.h>
 #include <unistd.h>
 
@@ -10,19 +11,31 @@ FILE *runSystemCommand(const char *system_comand);
  1. ensure that the command is right before trying to running the command
  2. I ran into a problem with "ps -aux" and "ps - aux" with cygwin
  3. locating substring in string "strstr()" function
+ 4. Return code of the process echo $? of piped process
+ 
+ example commands to run on RHEL and cygwin:
+ 1. "ps -aux"-> RHEL, "ps - aux" ->cygwin
+ 2. ls
 */
 int main(int argc, char** argv) {
 	FILE *fp;
 	char line[LINE_MAX];
+	int rc; /* Return code of the closed process*/
 	
 	printf("Welcome to run system command.\n");
-	fp = runSystemCommand("ps - aux");
 	
+	fp = runSystemCommand("ls");
+	
+	if(fp == NULL ){
+		perror("Unable to use popen with that command or error with popen.");
+		exit(EXIT_FAILURE);
+	}
 	/* char *fgets(char *s, int size, FILE *stream); */
 	while((fgets(line,LINE_MAX,fp)) != NULL){
 		printf("%s",line);
 	}
-	pclose(fp);
+	rc = pclose(fp);
+	printf("The return code for this runSystemCommand is: %d \n",rc);
 	return 0;
 }
 
