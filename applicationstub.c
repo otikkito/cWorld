89df24a1ea4266ba09b814e50a8f8d676f29dcb9
Kito Joseph <kitojoseph@gmail.com>
2019-11-26 19:16:17 -0500

    Corrected the formating of the applicationstub.c

--- applicationstub.c
+++ applicationstub.c
@@ -134,34 +134,34 @@ int printDebugInfo();
 *
 *********************************************************/
 int main(int argc, char** argv) {
-    /*As a design consideration minimize stuff in the main function for no particular reason other than readability and modulazation */
-    int i;
+	/*As a design consideration minimize stuff in the main function for no particular reason other than readability and modulazation */
+	int i;
 
 	/*Get the start time of the application to see how long the application has been running.*/
 	i = clock_gettime( CLOCK_MONOTONIC,&start_time);
-    if(i == -1){
-        perror("clock_gettime");
-        exit(EXIT_FAILURE);
-    }
+	if(i == -1){
+		perror("clock_gettime");
+		exit(EXIT_FAILURE);
+	}
 
-    i = atexit(bye);
-    if (i != 0) {
-        perror("Unable to set atexit()");
-        printLogFile(fp,"Unable to set atexit()");
-        exit(EXIT_FAILURE);
-    }
+	i = atexit(bye);
+	if (i != 0) {
+		perror("Unable to set atexit()");
+		printLogFile(fp,"Unable to set atexit()");
+		exit(EXIT_FAILURE);
+	}
 
-    /*The current pid of the process*/
-    processid = getpid(); /*Per the documentation this function is always successful*/
-    /*Open the log file to begin logging*/
-    fp = fopen(logfile, "a+"); /* TODO Change the name of fp and it is accessible throughout the file */
-    if(fp == NULL){
-        perror("Error with fopen(). Unable to open the application log");
-        exit(EXIT_FAILURE);
-    }
+	/*The current pid of the process*/
+	processid = getpid(); /*Per the documentation this function is always successful*/
+	/*Open the log file to begin logging*/
+	fp = fopen(logfile, "a+"); /* TODO Change the name of fp and it is accessible throughout the file */
+	if(fp == NULL){
+		perror("Error with fopen(). Unable to open the application log");
+		exit(EXIT_FAILURE);
+	}
 
-    printApplicationHeaderToConsole();
-    printLogFile(fp, "Application started.");
+	printApplicationHeaderToConsole();
+	printLogFile(fp, "Application started.");
 
 	int rc;
 	rc = readConfigurationFile();
@@ -175,19 +175,19 @@ int main(int argc, char** argv) {
 	}
 
 
-    /*
-     * Starting place of the application and application logic. Add code below 
-     * and remember to do proper logging and handling of errors by checking 
-     * return codes! The main function will return EXIT_SUCCESS or EXIT_FAILURE 
-     * depending on if there are any run-time errors in the application. The 
-     * application stub goal is to ensure that there are no compile errors thus 
-     * only having run-time errors which should be handled correctly and 
-     * prevented if that's possible.
-     * https://github.com/otikkito/cWorld/blob/master/applicationstub.txt
-     */
+	/*
+	* Starting place of the application and application logic. Add code below 
+	* and remember to do proper logging and handling of errors by checking 
+	* return codes! The main function will return EXIT_SUCCESS or EXIT_FAILURE 
+	* depending on if there are any run-time errors in the application. The 
+	* application stub goal is to ensure that there are no compile errors thus 
+	* only having run-time errors which should be handled correctly and 
+	* prevented if that's possible.
+	* https://github.com/otikkito/cWorld/blob/master/applicationstub.txt
+	*/
 
 
-    sleep(6000);
+	sleep(6000);
 
 	//printApplicationUptime();
 
@@ -221,12 +221,12 @@ int main(int argc, char** argv) {
 * 1) print also to the log file and console (stdout)
 *********************************************************/
 int printApplicationHeaderToConsole() {
-    printf("The process id of this application is: %d\n", processid);
-    printf("Welcome to the application stub.\nThe  purpose of this program ");
-    printf("is to build a solid framework \nfor the application development ");
-    printf("process.\n");
-    printf("----------------------------------------------------------------\n");
-    printf("The process name of this process is: %s \n", getProcessNameByPid(processid));
+	printf("The process id of this application is: %d\n", processid);
+	printf("Welcome to the application stub.\nThe  purpose of this program ");
+	printf("is to build a solid framework \nfor the application development ");
+	printf("process.\n");
+	printf("----------------------------------------------------------------\n");
+	printf("The process name of this process is: %s \n", getProcessNameByPid(processid));
 
 	return(EXIT_SUCCESS);
 }
@@ -270,7 +270,7 @@ int readConfigurationFile(){
 	//populate the global configuration structure
 	while((fgets(line,MAXCONFIGLINESIZE,config_file)) != NULL){
 	/*this needs to be corrected and/or add a special character to the front
-          of the configuration file indicating it is a configuration directive*/
+	of the configuration file indicating it is a configuration directive*/
 		if((line[0] == '#') || (line[0] == ' ' ) || (line[0] == '\n')) {  
 			continue;
 		}
@@ -329,11 +329,11 @@ int readConfigurationFile(){
 *
 *********************************************************/
 int printLogFile(FILE *f, char *string) {
-    char timestring[100];
-    time_t currenttime = time(0);
-    strftime(timestring, sizeof (timestring), "%c", localtime(&currenttime));
-    fprintf(f, "%s %s \n", timestring, string);
-    fflush(f);
+	char timestring[100];
+	time_t currenttime = time(0);
+	strftime(timestring, sizeof (timestring), "%c", localtime(&currenttime));
+	fprintf(f, "%s %s \n", timestring, string);
+	fflush(f);
 
 	return(EXIT_SUCCESS);
 }
@@ -467,19 +467,19 @@ int initializeSignalHandles(){
 *
 *********************************************************/
 void signalHandler(int signal, siginfo_t *info, void *_unused) {
-    /*
-     * http://man7.org/linux/man-pages/man2/sigaction.2.html --> "You must set the sa_flags field"
-     * "you must set the sa_flags field of your struct sigaction with the flag SA_SIGINFO"
-     * https://stackoverflow.com/questions/15148714/signals-siginfo-t-info-causes-segmentation-fault
+	/*
+	* http://man7.org/linux/man-pages/man2/sigaction.2.html --> "You must set the sa_flags field"
+	* "you must set the sa_flags field of your struct sigaction with the flag SA_SIGINFO"
+	* https://stackoverflow.com/questions/15148714/signals-siginfo-t-info-causes-segmentation-fault
 	 *read man signal.h
-     */
-    char app_log_message[MAXLOGENTRYSIZE];
+	 */
+	char app_log_message[MAXLOGENTRYSIZE];
 
-    memset(app_log_message,'\0',sizeof(app_log_message));
-    /*siginfo_t not returning properly*/
-    sprintf(app_log_message,"The application received signal %d from pid: %u with process name %s",signal,info->si_pid, getProcessNameByPid(info->si_pid));
-    /*Log to the application log the signal*/
-    printLogFile(fp,app_log_message);
+	memset(app_log_message,'\0',sizeof(app_log_message));
+	/*siginfo_t not returning properly*/
+	sprintf(app_log_message,"The application received signal %d from pid: %u with process name %s",signal,info->si_pid, getProcessNameByPid(info->si_pid));
+	/*Log to the application log the signal*/
+	printLogFile(fp,app_log_message);
 
 	/*To terminate kill -s 15 <pid>*/
 	/*man 7 signal*/
@@ -528,10 +528,10 @@ void signalHandler(int signal, siginfo_t *info, void *_unused) {
  *********************************************************/
 /* can also be done be running ps -p PID -i comm= */
 const char* getProcessNameByPid(pid_t pid) {
-    FILE *f;
-    char* name = (char*) calloc(1024, sizeof (char));
-    //ubuntu
-    //Need to determine if RHEL 7 or 6 is being used. /etc/redhat-release
+	FILE *f;
+	char* name = (char*) calloc(1024, sizeof (char));
+	//ubuntu
+	//Need to determine if RHEL 7 or 6 is being used. /etc/redhat-release
 	if(pid == 0){
 	        return "Kernel"; //This is the abstraction point...better pin pointing.
   	}
@@ -540,20 +540,21 @@ const char* getProcessNameByPid(pid_t pid) {
 	    return "Systemd";
 	}
 
-    if (name) {
-        sprintf(name, "/proc/%d/cmdline", pid);
-        f = fopen(name, "r");
-        if (f) {
-            size_t size;
-            size = fread(name, sizeof (char), 1024, f);
-            if (size > 0) {
-                if ('\n' == name[size - 1])
-                    name[size - 1] = '\0';
-            }
-            fclose(f);
-        }
-    }
-    return name;
+	if (name) {
+		sprintf(name, "/proc/%d/cmdline", pid);
+		f = fopen(name, "r");
+		
+		if (f) {
+			size_t size;
+			size = fread(name, sizeof (char), 1024, f);
+				if (size > 0) {
+					if ('\n' == name[size - 1])
+						name[size - 1] = '\0';
+					}
+			fclose(f);
+		}
+	}
+	return name;
 }
 
 /********************************************************
@@ -586,15 +587,15 @@ int printApplicationUptime(){
 
 	i = clock_gettime( CLOCK_MONOTONIC,&elapsed_time);
 	if(i == -1){
-        	perror("clock_gettime");
-        	exit(EXIT_FAILURE);
-        }
+		perror("clock_gettime");
+		exit(EXIT_FAILURE);
+	}
 
 	mins = (elapsed_time.tv_sec - start_time.tv_sec)/60;
 	secs = (elapsed_time.tv_sec - start_time.tv_sec)%60;
 	sprintf(logentry,"This application has an uptime of %d mins and %d seconds",mins,secs);
 	printLogFile(fp,logentry);
-        printf("%s\n",logentry);
+	printf("%s\n",logentry);
 	//need to add variadic arguments for the printlogfile.
 	//printLogFile(fp,"The application has been up for %d mins"
 
