ff5e47e0fe70c90e79da86844d8631678507de16
Kito Joseph <kitojoseph@gmail.com>
2019-12-08 21:57:07 -0500

    Started working on a file that will  access a datbase and create dyanmic data for website on the learning laptop.

---
+++ apps/databaseapp/dbapp.c
@@ -0,0 +1,76 @@
+/****************************************************************
+*FILE NAME: dbapp.c
+*
+*
+*
+*PURPOSE: To develope a program that access a database and use proper self documentation.
+* 
+*
+*FILE REFERENCES:
+*
+*NAME		I/O		Description
+*----           ---             -----------
+*
+*EXTERNAL VARIABLE:
+*SOURCE: <         >
+*
+*NAME	  TYPE    	I/O		Description
+*----     ----          ---             -----------
+*
+*EXTERNAL REFERENCES:
+*
+*Name		Description
+*----   	-----------
+*
+*ABNORMAL TERMINATION CONDITIONS, ERROR AND WARNING MESSAGES:
+*
+*ASSUMPTIONS, CONSTRAINTS, RESTRICTIONS
+*
+*NOTES:
+*
+*REQUIREMENTS/FUNCTIONAL SPECIFICATIONS REFERENCES:
+*
+*DEVELOPMENT HISTORY:
+*
+*Date		Author		Change ID	Release		Description of Change
+*----   	------  	---------   -------     ---------------------
+*
+*ALGORITHM (PDL)
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Notes
+
+
+*/
+
+
+/********************************************************
+*
+*
+* FUNCTION NAME: main (the entry point of the program)
+*
+*
+*
+* ARGUMENTS:
+*
+*
+*
+* ARGUMENT     TYPE I/O DESCRIPTION
+* --------     ---- --- -----------
+*
+*
+*
+* RETURNS:
+*
+*
+*
+*********************************************************/
+int main(int argc, char** argv) {
+
+	printf("Welcome to dbapp\n");
+
+	return EXIT_SUCCESS;
+}
