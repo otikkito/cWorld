082679934a6025d8b273e9bdd894f82d1d53cde5
Kito Joseph <kitojoseph@gmail.com>
2020-12-06 16:42:44 -0500

    Updated the index page.

--- databases/website/index.php
+++ databases/website/index.php
@@ -1,8 +1,7 @@
 <html>
 <!-- https://codular.com/php-mysqli -->
 <!-- https://www.w3schools.com/php/php_ref_mysqli.asp -->
-<!-- https://www.websiteout.net/  -->
-<!-- databases setup: https://github.com/otikkito/cWorld/blob/master/databases/notes.txt -->
+<!--- https://www.websiteout.net/  -->
 <head>
 <title>Kito Joseph's website</title>
 </head>	
@@ -46,22 +45,25 @@ $db->close();
 
 <br>
 <a href="https://bugzilla.redhat.com/">Red Hat Bugzilla.</a><br><br><br>
+<a href="KitoJosephResumeMCC.pdf">Kito's resume</a><br><br>
+<a href="/workpics">Work-Datacenter-pictures</a>&nbsp;<a href="/botanicgarden">Juniper-level-botanic-garden-pictures</a>
+<a href="/biltmore-estate">Biltmore-Estate-pictures</a> <br><br>
 <?php
 echo "Today's date is: " . date("m/d/Y") . "<br>";
 ?>
-<p><a href="https://github.com/otikkito/cWorld/blob/master/databases/notes.txt">Code for this page</a<br>
+<p><a href="https://github.com/otikkito/cWorld/blob/master/databases/website/index.php">Code for this page</a<br>
 <p><a href="https://www.worldofmolecules.com/antioxidants/vitaminc.htm">Vitamin C</a></p>
 <div data-iframe-width="150" data-iframe-height="270" data-share-badge-id="8419f706-1f58-466b-9349-7d419b16d7dc" data-share-badge-host="https://www.youracclaim.com"></div><script type="text/javascript" async src="//cdn.youracclaim.com/assets/utilities/embed.js"></script>
 <div data-iframe-width="150" data-iframe-height="270" data-share-badge-id="4e254be8-ddf6-4050-a4cc-fb995c2b1c1b" data-share-badge-host="https://www.youracclaim.com"></div><script type="text/javascript" async src="//cdn.youracclaim.com/assets/utilities/embed.js"></script>
 <div data-iframe-width="150" data-iframe-height="270" data-share-badge-id="12cf5260-bece-4239-bc29-91622f6b3203" data-share-badge-host="https://www.youracclaim.com"></div><script type="text/javascript" async src="//cdn.youracclaim.com/assets/utilities/embed.js"></script>
-
+<div data-iframe-width="150" data-iframe-height="270" data-share-badge-id="ee78551e-3122-4b50-8d75-31fb6f9278aa" data-share-badge-host="https://www.youracclaim.com"></div><script type="text/javascript" async src="//cdn.youracclaim.com/assets/utilities/embed.js"></script>
 <!-- hitwebcounter Code START -->
 <a href="https://www.hitwebcounter.com" target="_blank">
 <img src="https://hitwebcounter.com/counter/counter.php?page=7439199&style=0001&nbdigits=5&type=page&initCount=0" title="Web Counter" Alt="counter free"   border="0" >
 </a>                 
 
 <script type="text/javascript" src="//counter.websiteout.net/js/22/0/0/1"></script>	
-<p> Also check out <a href="http://www.kjoseph-it.com">www.kjoseph-it.com</a> , <a href="http://www.linkedin.com/in/kitojoseph">www.linkedin.com/in/kitojoseph</a></p>
+<p> Also check out <a href="http://www.kjoseph-it.com">www.kjoseph-it.com</a> , <a href="http://www.linkedin.com/in/kitojoseph">www.linkedin.com/in/kitojoseph</a>, <a href="http://www.ganssle.com/"> Embedded Systems</a></p>
 <p>Please dontate to keep these sites alive.<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
 <input type="hidden" name="cmd" value="_donations" />
 <input type="hidden" name="business" value="T84TGJBVD5WUN" />
