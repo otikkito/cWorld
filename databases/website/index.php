d253475f20157051142c2a9a414a62afc9f57908
Kito Joseph <kitojoseph@gmail.com>
2021-03-03 17:26:01 -0500

    Update index.php
    Changed the sql statment to include "ro".

--- databases/website/index.php
+++ databases/website/index.php
@@ -81,7 +81,7 @@ echo "Today's date is: " . date("m/d/Y") . "<br>";
     		die('Unable to connect to database [' . $db->connect_error . ']');
 	}
 
-	$sql = "SELECT * from personlist where lname like 'ra%';";	
+	$sql = "SELECT * from personlist where lname like 'ro%';";	
 	
 	if(!$result = $db->query($sql)){
 	    die('There was an error running the query [' . $db->error . ']');
