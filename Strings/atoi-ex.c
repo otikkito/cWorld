35cf4b05eca9ab77f5d7f46fcbe7c3632767618c
Kito Joseph <kitojoseph@gmail.com>
2022-07-09 16:20:55 -0400

    Update atoi-ex.c

--- Strings/atoi-ex.c
+++ Strings/atoi-ex.c
@@ -7,9 +7,9 @@ int main(){
 	
 	printf("Welecome to the atoi example.\n");
 	printf("Ex.1 : %d\n",atoi("5555"));
-	printf("Ex.1 : %d\n",atoi("5555aq"));
-	printf("Ex.2 : %d\n",atoi("55gth55"));
-	printf("Ex.3 : %d\n",atoi("rgf555fgf"));
+	printf("Ex.2 : %d\n",atoi("5555aq"));
+	printf("Ex.3 : %d\n",atoi("55gth55"));
+	printf("Ex.4 : %d\n",atoi("rgf555fgf"));
 	
 	return 0;
 	
